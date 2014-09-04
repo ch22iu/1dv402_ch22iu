71ba60c3a34ebbb1d5cc128817df2b77dbfb8306
Christopher <ch22iu@student.lnu.se>
2014-09-04 10:50:49 +0200

    StartValue

--- moneyexchange_v37/Program.cs
+++ moneyexchange_v37/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.IO;
 
 namespace moneyexchange_v37
 {
@@ -10,9 +12,47 @@ namespace moneyexchange_v37
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
-            Console.ReadKey();
-
+            
+        }
+        private static double ReadPositiveDouble(string prompt = null)
+        {
+            double startValue = 0;
+            bool startReading = true;
+            while (startReading)
+            {
+                {
+                    // Skriver ut..
+                    if (prompt != null)
+                    {
+                        Console.Write(prompt);
+                    }
+                    startValue = 0;
+                    // Kollar upp och byter ut värde.
+                    if (Double.TryParse(Console.ReadLine().Replace('.', ','), out startValue))
+                    {
+                        double fixValue = Math.Round(startValue, MidpointRounding.AwayFromZero);
+                        if (fixValue > 0)
+                        {
+                            startReading = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error! '{0}' to small value.", startValue);
+                            Console.ReadLine();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error! Error!");
+                        Console.ReadLine();
+                    }
+                }
+            }
+            return startValue;
+        }
+        private static uint ReadUint(string prompt, uint minValue)
+        {
+            return (50);
         }
     }
 }
