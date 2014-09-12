26ece9dfad223a0c7eef14d2d223fac5d548d843
Christopher <ch22iu@student.lnu.se>
2014-09-12 11:20:16 +0200

    Added my methods for V1_2

--- labbar/V1/V1.2/writeWithAst_v38/writeWithAst_v38/Program.cs
+++ labbar/V1/V1.2/writeWithAst_v38/writeWithAst_v38/Program.cs
@@ -11,6 +11,24 @@ namespace writeWithAst_v38
         private const byte max = 79;
         static void Main(string[] args)
         {
+            byte columns = 0;
+
+            do
+            {
+
+            } while();
+
+        }
+        private static byte ReadOddByte(string prompt = null, byte maxValue = max)
+        {
+            return;
+        }
+        private static void Render(byte maxCount)
+        {
+
+        }
+        static void rows(int maxCount, int aCount)
+        {
 
         }
     }
