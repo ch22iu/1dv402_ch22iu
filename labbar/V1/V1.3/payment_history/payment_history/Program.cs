ea9c32aee37d979543bc50e1e8ee04557d9978a5
ch22iu <ch22iu@student.lnu.se>
2014-09-23 09:41:16 +0200

    Adding methods and value inputs

--- labbar/V1/V1.3/payment_history/payment_history/Program.cs
+++ labbar/V1/V1.3/payment_history/payment_history/Program.cs
@@ -8,28 +8,107 @@ namespace payment_history
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            int numberOfSalaries;
+            int[] salaries;
+
+            do
+            {
+                do
+                {
+                    numberOfSalaries = ReadInt("How many salaries: ");
+                    if(numberOfSalaries < 2)
+                    {
+                        ViewMessage(strings.isError, ConsoleColor.Green, ConsoleColor.White);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    while (true);
+
+                    salaries = ReadSalaries(numberOfSalaries);
+                    ViewResult();
+                } while (isContinuing());
+           
+        
+        private static bool isContinuing()
+        {
+            {
+                ViewMessage(Console.Write(strings.isError), ConsoleColor.Red, ConsoleColor.Blue)
+            }
+         return true;
+        }
+        private static bool isContinuing()
+        {
+            ViewMessage(strings.valueError, ConsoleColor.Red,ConsoleColor.Blue);
+
+            if (Console.ReadKey(true).Key != ConsoleKey.Escape)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+           
+        }
+
+        private static int[] ReadSalaries(int count)
+        {
+            List<int> salaries = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                salaries.Add(ReadInt(String.Format("Pay {0}: ", i)));
+            }
+                return salaries.ToArray();
         }
         private static int ReadInt(string prompt)
         {
-            return 0;
+            int number = 0;
+            string input;
+            bool success = false;
+
+            while (!success)
+            {
+                try
+                {
+
+                }
+                catch
+                {
+                    ViewMessage(strings.valueError, ConsoleColor.Red, ConsoleColor.White);
+                }
+            }
+            return number;
         }
-        private static int ReadInt(string prompt)
+        private static void ViewMessage(string message, ConsoleColor backgroundColor, ConsoleColor textColor)
         {
-            return 0;
+            Console.WriteLine();
+            Console.BackgroundColor = backgroundColor;
+            Console.ForegroundColor = textColor;
+            Console.WriteLine();
+            Console.ResetColor();
         }
-        private static int ReadInt(string prompt)
+        private void ViewResult(int[] salaries)
         {
-            return 0;
+            Console.WriteLine();
+            Console.WriteLine("----------------------");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("----------------------");
+            for (int i = 0; i + j < salaries.Length && j < 3; j++)
+            {
+                Console.Write("{0,7}, salaries[i + j])"
+            }
         }
-        private static int ReadInt(string prompt)
-        {
-            return 0;
-        }
-        private static int ReadInt(string prompt)
-        {
-            return 0;
-        }
+
     }
 }
---
+++ labbar/V1/V1.3/payment_history/payment_history/myExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace payment_history
+{
+    public static class myExtensions
+    {
+        public static int Dispertion(this int[] source)
+        {
+            return source.Max() - source.Min();
+        }
+        public static Median(this int[] source)
+        {
+            List<int> values = new List<int>(source);
+            values.Sort();
+
+            while (values.Count() > 2)
+            {
+                values.RemoveAt(0);
+                values.RemoveAt(values.Count() -1);
+            }
+
+            if (values.Count == 2)
+            {
+                return (int)Math.Round(values.Average());
+                
+            }
+            else
+            {
+                return values[0];
+            }
+        }
+    }
+}
