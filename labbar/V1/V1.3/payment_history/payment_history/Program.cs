c6e4585ef8d3125f94acfadbf0696db1a0ef7d2c
ch22iu <ch22iu@student.lnu.se>
2014-09-23 08:31:59 +0200

    Adding methods

---
+++ labbar/V1/V1.3/payment_history/payment_history/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace payment_history
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        private static int ReadInt(string prompt)
+        { 
+        
+        }
+        private static int ReadInt(string prompt)
+        {
+
+        }
+        private static int ReadInt(string prompt)
+        {
+
+        }
+        private static int ReadInt(string prompt)
+        {
+
+        }
+        private static int ReadInt(string prompt)
+        {
+
+        }
+    }
+}
