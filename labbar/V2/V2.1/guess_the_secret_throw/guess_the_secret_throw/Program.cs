bf0623700a7f3f101eb90c6c3800250b9b28c7d4
ch22iu <ch22iu@student.lnu.se>
2014-09-30 09:16:11 +0200

    Adding 2010 VB

---
+++ labbar/V2/V2.1/guess_the_secret_throw/guess_the_secret_throw/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace guess_the_secret_throw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test! ");
+            Console.ReadLine();
+        }
+    }
+}
