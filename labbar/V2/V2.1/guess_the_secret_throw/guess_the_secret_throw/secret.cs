442339aa2426751ebc166ee51a474775cfca039e
ch22iu <ch22iu@student.lnu.se>
2014-09-30 09:22:37 +0200

    Adding 2010 VB

---
+++ labbar/V2/V2.1/guess_the_secret_throw/guess_the_secret_throw/secret.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace guess_the_secret_throw
+{
+    public class secret
+    {
+        public int _faceValue;
+
+        public int Throw()
+        {
+            Random mRandom = new Random();
+            _faceValue = mRandom.Next(1, 7);
+
+            return _faceValue;
+        }
+    }
+}
