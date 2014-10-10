cd3b43e15891190e64ed8d8e2bd258f410868b7a
Christopher <ch22iu@student.lnu.se>
2014-10-10 20:15:19 +0200

    Adding metods to recept

---
+++ labbar/V3/V3.1/LB3_ReceptPaFil/LB3_ReceptPaFil/IRecipeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB3_ReceptPaFil
+{
+    interface IRecipeRepository
+    {
+        static void Delete(int index)
+        {
+
+        }
+        
+    }
+}
---
+++ labbar/V3/V3.1/LB3_ReceptPaFil/LB3_ReceptPaFil/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB3_ReceptPaFil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
